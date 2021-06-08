9f7614e252d3aca1f3047a7fcdfc9e73d706e1dd
DiegoEmanuel <diegoabox@gmail.com>
2021-06-07 22:42:27 -0300

    Layouts e bootstrap e fonts google

--- resources/views/products.blade.php
+++ resources/views/products.blade.php
@@ -1,1 +1,9 @@
-Paginas de produtos
+@extends('layout.main')
+@section('title','Doacoes')
+@section('content')
+
+
+
+<h1>Paginas de produtos</h1>
+
+@endsection
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,29 +1,17 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+@extends('layout.main')
+@section('title', 'AJUDAI')
+@section('content')
 
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-
-    <title>Laravel</title>
-    <link rel="stylesheet" href="/css/style.css">
-    <script src="/js/scripts.js"></script>
-
-
-</head>
-
-<body class="antialiased">
     <h1> Alguma coisa </h1>
-    <img src="/images/logoajudai.png" alt="banner"/>
+    <img src="/images/logoajudai.png" alt="banner" />
     @if (10 > 9)
         <p>A condição é true</p>
 
     @endif
     <p>O nome é {{ $nome }}
 
-    @for ($i=0;$i < count($arr); $i++)
-        <p>{{$arr[$i]}} - {{$i}} </p>
-    @endfor
-</body>
+        @for ($i = 0; $i < count($arr); $i++)
+            <p>{{ $arr[$i] }} - {{ $i }} </p>
+        @endfor
 
-</html>
+    @endsection
