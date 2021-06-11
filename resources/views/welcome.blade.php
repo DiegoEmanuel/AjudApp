38c38c860abe3c0d5f53a6204797ddb58fe9f343
DiegoEmanuel <diegoabox@gmail.com>
2021-06-10 23:18:07 -0300

    view-css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,7 +1,29 @@
 @extends('layout.main')
 @section('title', 'AJUDAI')
 @section('content')
-    @foreach ($events as $event)
-        <p> {{ $event->title }} -- {{ $event->description }}</p>
-    @endforeach
+
+    <div id="seach-container" class="col-md-12">
+        <h1>Busque um evento</h1>
+        <form action="">
+            <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
+
+        </form>
+    </div>
+    <div id="events-container" class="col-md-12">
+        <h2>Próximos eventos</h2>
+        <p>Veja os eventos dos proximos dias</p>
+        <div id="cards-container" class="row">
+            @foreach ($events as $event)
+                <div class="card col-md-3">
+                    <img src="/images/logoajudai.png" alt="titulo">
+                    <div class="card-body">
+                        <p class="card-date">10-06-2021</p>
+                        <h5 class="card-title"></h5>
+                        <p class="card-participants">X Participantes</p>
+                        <a href="#" class="btn btn-primary">Saber mais</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
+    </div>
 @endsection
