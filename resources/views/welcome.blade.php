304a6e1b821a6eab382c22dc3712ee8a266e48eb
DiegoEmanuel <diegoabox@gmail.com>
2021-06-11 00:06:50 -0300

    Alteraçoes no css

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -2,8 +2,8 @@
 @section('title', 'AJUDAI')
 @section('content')
 
-    <div id="seach-container" class="col-md-12">
-        <h1>Busque um evento</h1>
+    <div id="search-container" class="col-md-12">
+        <h1>Procurar doação</h1>
         <form action="">
             <input type="text" id="search" name="search" class="form-control" placeholder="Procurar">
 
@@ -11,14 +11,14 @@
     </div>
     <div id="events-container" class="col-md-12">
         <h2>Próximos eventos</h2>
-        <p>Veja os eventos dos proximos dias</p>
+        <p class="subititle">Veja os eventos dos proximos dias</p>
         <div id="cards-container" class="row">
             @foreach ($events as $event)
                 <div class="card col-md-3">
                     <img src="/images/logoajudai.png" alt="titulo">
                     <div class="card-body">
                         <p class="card-date">10-06-2021</p>
-                        <h5 class="card-title"></h5>
+                        <h5 class="card-title">{{$event->title}}</h5>
                         <p class="card-participants">X Participantes</p>
                         <a href="#" class="btn btn-primary">Saber mais</a>
                     </div>
