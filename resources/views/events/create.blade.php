8e887353bdf2529c0a842bc014c2b44f6e2d345b
DiegoEmanuel <diegoabox@gmail.com>
2021-06-11 22:09:55 -0300

    form

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -2,6 +2,24 @@
 @section('title','Doacoes')
 @section('content')
 
-<h1>crie um evento</h1>
+<div id="event-create-container" class="col-md-5 offset md-3">
+    <h1>Crie a sua doação</h1>
+    <form action="/events" method="POST">
+        <div class="form-group">
+            <label for="title"></label>
+            <input type="text"class="form-control" id="title" name="title" placeholder="Nome do evento">
+        </div>
+        <div class="form-group">
+            <label for="Cidade"></label>
+            <input type="text"class="form-control" id="city" name="city" placeholder="Local">
+        </div>
+        <div class="form-group">
+            <label for="title"></label>
+            <input type="text"class="form-control" id="title" name="title" placeholder="Nome do evento">
+        </div>
+
+    </form>
+
+</div>
 
 @endsection
