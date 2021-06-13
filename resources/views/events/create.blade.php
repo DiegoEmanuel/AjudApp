c109af15d898c737d9fc810abfb171308a275aea
DiegoEmanuel <diegoabox@gmail.com>
2021-06-13 20:30:09 -0300

    Melhorias e criar evento implementado

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -1,32 +1,34 @@
 @extends('layout.main')
-@section('title','Doacoes')
+
+@section('title', 'Criar Evento')
+
 @section('content')
 
-<div id="event-create-container" class="col-md-5 offset md-3">
-    <h1>Crie a sua doação</h1>
-    <form action="/events" method="POST">
-        <div class="form-group">
-            <label for="title"></label>
-            <input type="text"class="form-control" id="title" name="title" placeholder="Nome do evento">
-        </div>
-        <div class="form-group">
-            <label for="Cidade"></label>
-            <input type="text"class="form-control" id="city" name="city" placeholder="Local">
-        </div>
-        <div class="form-group">
-            <label for="O evento é privado ?"></label>
-            <select name="private" id="private" class="form-control">
-                <option value="0">Não</option>
-                <option value="1">Sim</option>
-            </select>
-        </div>
-        <div class="form-group">
-            <label for="Descrição"></label>
-            <textarea name="description" placeholder="O evento será" id="description" class="form-control"></textarea>
-        </div>
-
-    </form>
-    <input type="submit" class="btn btm-primary" value="criar evento">
+<div id="event-create-container" class="col-md-6 offset-md-3">
+  <h1>Crie o seu evento</h1>
+  <form action="/events" method="POST">
+    @csrf
+    <div class="form-group">
+      <label for="title">Evento:</label>
+      <input type="text" class="form-control" id="title" name="title" placeholder="Nome do evento">
+    </div>
+    <div class="form-group">
+      <label for="title">Cidade:</label>
+      <input type="text" class="form-control" id="city" name="city" placeholder="Local do evento">
+    </div>
+    <div class="form-group">
+      <label for="title">O evento é privado?</label>
+      <select name="private" id="private" class="form-control">
+        <option value="0">Não</option>
+        <option value="1">Sim</option>
+      </select>
+    </div>
+    <div class="form-group">
+      <label for="title">Descrição:</label>
+      <textarea name="description" id="description" class="form-control" placeholder="O que vai acontecer no evento?"></textarea>
+    </div>
+    <input type="submit" class="btn btn-primary" value="Criar Evento">
+  </form>
 </div>
 
 @endsection
