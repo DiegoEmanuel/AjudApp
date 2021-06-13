648905237146fdbefc245deb7d99a53dc0d0ec30
DiegoEmanuel <diegoabox@gmail.com>
2021-06-12 23:54:29 -0300

    Commit

--- resources/views/events/create.blade.php
+++ resources/views/events/create.blade.php
@@ -14,12 +14,19 @@
             <input type="text"class="form-control" id="city" name="city" placeholder="Local">
         </div>
         <div class="form-group">
-            <label for="title"></label>
-            <input type="text"class="form-control" id="title" name="title" placeholder="Nome do evento">
+            <label for="O evento é privado ?"></label>
+            <select name="private" id="private" class="form-control">
+                <option value="0">Não</option>
+                <option value="1">Sim</option>
+            </select>
+        </div>
+        <div class="form-group">
+            <label for="Descrição"></label>
+            <textarea name="description" placeholder="O evento será" id="description" class="form-control"></textarea>
         </div>
 
     </form>
-
+    <input type="submit" class="btn btm-primary" value="criar evento">
 </div>
 
 @endsection
